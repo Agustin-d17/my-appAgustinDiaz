7d29c1ae3025f6da93eac8949834872451de0ed0
agustin-d17 <agustindiaz449@gmail.com>
2022-05-24 17:54:10 -0300

    correccion de rutas

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -2,6 +2,7 @@ import BuyButtons from './BuyButtons';
 import ItemCount from './ItemCount';
 import {useState} from 'react'
 import './main.css'
+import {Link} from 'react-router-dom'
 
 function ItemDetail ({item}) {
     const [inputType, setInputType] = useState('itemCount')
@@ -15,13 +16,16 @@ function ItemDetail ({item}) {
     }
 
     return <div className="item-detail">
-                <img className="item-detail-img" src={item.imagenB} alt={item.name}/>
+                <img className="item-detail-img" src={item.imagenA} alt={item.name}/>
                 <div className="item-detail-info">
+                    <div className="btn-close-container">
+                        <Link to="/"><button className="btn-close"><span className="material-icons">close</span></button></Link>
+                    </div>
                     <h3>{item.name}</h3>
                     <p className="detalles-producto">{item.detail}</p>
                     <span>${item.precio}</span>
                     {
-                        inputType == 'itemCount' ? 
+                        inputType === 'itemCount' ? 
                         <ItemCount stock={item.stock} initial="1" onAdd={infoAgregados} handleInputType={handleInputType}/> : 
                         <BuyButtons />
                     }                    
